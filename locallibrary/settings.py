71ece51a7404b6c3c338122d850b87691061f23c
yss <shreyank.shukla.rishu@gmail.com>
2020-04-15 11:18:12 +0530

    Updated ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['quiet-spire-31078.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
